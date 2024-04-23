63ba60c22c696ebaf4e5a5e7cee634775cd7b7b1
Catherine  Hull <cathh127@hotmail.com>
2024-04-23 12:49:57 +0000

    All DOM elements added

---
+++ js/questions.js
@@ -0,0 +1,108 @@
+const questions = [
+    {
+        question: "What is the average lifespan of sloths in the wild?",
+    
+        answers: [
+          { text: "15-20 years", correct: false },
+          { text: "20-25 years", correct: true },
+          { text: "30-35 years", correct: false },
+          { text: "40-45 years", correct: false },
+        ],
+    },
+    {
+        question: "How many hours does it take for a sloth to digest a meal?",
+    
+        answers: [
+          { text: "6-8 hours", correct: false },
+          { text: "12-24 hours", correct: false },
+          { text: "36-48 hours", correct: true, },
+          { text: "72-96 hours", correct: false },
+        ],
+    },
+    {
+        question: "Which of the following is true about sloths fur?",
+    
+        answers: [
+          { text: "It is naturally blue in colour", correct: false },
+          { text: "It grows in the opposite direction to other mammals", correct: true },
+          { text: "It sheds once a year", correct: false },
+          { text: "It is resistant to parasites", correct: false },
+        ],
+    },
+  
+    {
+        question: "What is the weight of a newborn sloth?",
+    
+        answers: [
+          { text: "1-2 pounds", correct: true },
+          { text: "3-5 pounds", correct: false },
+          { text: "6-8 pounds", correct: false },
+          { text: "9-11 pounds", correct: false },
+        ],
+    },
+  
+    {
+        question: "Who is a natural predator of sloths?",
+    
+        answers: [
+          { text: "jaguars", correct: true },
+          { text: "elephants", correct: false },
+          { text: "crocodiles", correct: false },
+          { text: "gorillas", correct: false },
+        ],
+    },
+  
+    {
+        question: "Where are sloths found in the wild?",
+    
+        answers: [
+          { text: "Africa", correct: false },
+          { text: "Asia", correct: false },
+          { text: "Australia", correct: false },
+          { text: "Central and South America", correct: true },
+        ],
+    },
+  
+    {
+        question: "How many hours a day do sloths sleep?",
+    
+        answers: [
+          { text: "8-10 hours", correct: false },
+          { text: "10-12 hours", correct: false },
+          { text: "12-14 hours", correct: true },
+          { text: "14-16 hours", correct: false },
+        ],
+    },
+  
+    {
+        question: "How many chambers does a sloth stomach have?",
+    
+        answers: [
+          { text: "one", correct: false },
+          { text: "two", correct: true },
+          { text: "three", correct: false },
+          { text: "four", correct: false },
+        ],
+    },
+  
+    {
+        question: "Which sense is the best for sloths?",
+    
+        answers: [
+          { text: "sight", correct: false },
+          { text: "taste", correct: false },
+          { text: "touch", correct: false },
+          { text: "smell", correct: true },
+        ],
+    },
+    {
+        question: "What is the main diet of a sloth?",
+    
+        answers: [
+          { text: "insects", correct: false },
+          { text: "meat", correct: false },
+          { text: "leaves", correct: true },
+          { text: "fish", correct: false },
+        ],
+    }
+  ]
---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Selecting required elements //
+const startBtn = document.querySelector('.start-btn');
+const infoBox = document.querySelector('.info-box');
+const ExitBtn = document.querySelector('.buttons .quit');
+const ContinueBtn = document.querySelector('.buttons .restart');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result.box');
+const optionList = document.querySelector('.option-list');
+const timeLine = document.querySelector('.timer .time-line');
+const timeText = document.querySelector('.timer .time-left-text');
+const timeCount = document.querySelector('.timer .timer-sec');
