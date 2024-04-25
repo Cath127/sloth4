d47b1e519b24973e0db78dd29cc54b34ff376604
Catherine  Hull <cathh127@hotmail.com>
2024-04-25 09:24:41 +0000

    My questions working

--- js/questions.js
+++ js/questions.js
@@ -1,9 +1,11 @@
+// creating an array and passing the number, questions, options, and answers
+
 let questions = [
   {
-      numb:1
-        question: 'What is the average lifespan of sloths in the wild?',
-        answer: '20-25 years',
-        options: [
+   numb: 1,
+   question: 'What is the average lifespan of sloths in the wild?',
+   answer: '20-25 years',
+   options: [
           '15-20 years',
           '20-25 years',
           '30-35 years',
@@ -11,32 +13,32 @@ let questions = [
         ]
     },
     {
-      numb:2
-        question: 'How many hours does it take for a sloth to digest a meal?',
-        answer: '36-48 hours',
-        options: [
+    numb:2,
+    question: 'How many hours does it take for a sloth to digest a meal?',
+    answer: '36-48 hours',
+    options: [
           '6-8 hours',
           '12-24 hours',
           '36-48 hours', 
           '72-96 hours'
-        ]
-    },
-    {
-      numb:3
-        question: 'Which of the following is true about sloths fur?',
-        answer: 'It grows in the opposite direction to other mammals',
-        options: [
-          'It is naturally blue in colour',
-          'It grows in the opposite direction to other mammals',
-          'It sheds once a year',
-          'It is resistant to parasites'
-        ]
-    }, 
-    {
-      numb:4
-        question: 'What is the weight of a newborn sloth?',
-        answer: '1-2 pounds',
-        options: [
+              ]
+          },
+          {
+    numb:3,
+    question: 'Which of the following is true about sloths fur?',
+    answer: 'It grows in the opposite direction to other mammals',
+    options: [
+            'It is naturally blue in colour',
+            'It grows in the opposite direction to other mammals',
+            'It sheds once a year',
+            'It is resistant to parasites'
+              ]
+                }, 
+            {
+    numb:4,
+    question: 'What is the weight of a newborn sloth?',
+    answer: '1-2 pounds',
+    options: [
           '1-2 pounds',
           '3-5 pounds', 
           '6-8 pounds',
@@ -44,7 +46,7 @@ let questions = [
         ]
     },
     {
-      numb:5
+      numb:5,
         question: 'Who is a natural predator of sloths?',
         answer: 'jaguars',
         options: [
@@ -55,18 +57,18 @@ let questions = [
         ]
     },
     {
-      numb:6
+      numb:6,
         question: 'Where are sloths found in the wild?',
         answer: 'Central and South America',
         options: [
-          'Africa'
-          'Asia'
-          'Australia'
+          'Africa',
+          'Asia',
+          'Australia',
           'Central and South America'
         ]
     },
     {
-      numb:7
+      numb:7,
         question: 'How many hours a day do sloths sleep?',
         answer: '12-14 hours',
         options: [
@@ -77,7 +79,7 @@ let questions = [
         ]
     },
     {
-      numb:8
+      numb:8,
         question: 'How many chambers does a sloth stomach have?',
         answer: 'two',
         options: [
@@ -88,9 +90,9 @@ let questions = [
         ]
     },
     {
-      numb:9
+      numb:9,
         question: 'Which sense is the best for sloths?',
-        answer: 'smell'
+        answer: 'smell',
         options: [
           'sight',
           'taste',
@@ -99,7 +101,7 @@ let questions = [
         ]
     },
     {
-      numb:10
+      numb:10,
         question: 'What is the main diet of a sloth?',
         answer: 'leaves',
         options:
@@ -111,115 +113,3 @@ let questions = [
         ]
     }
   ]
-
-
-
-
-// const questions = [
-//     {
-//         question: "What is the average lifespan of sloths in the wild?",
-    
-//         answers: [
-//           { text: "15-20 years", correct: false },
-//           { text: "20-25 years", correct: true },
-//           { text: "30-35 years", correct: false },
-//           { text: "40-45 years", correct: false },
-//         ],
-//     },
-//     {
-//         question: "How many hours does it take for a sloth to digest a meal?",
-    
-//         answers: [
-//           { text: "6-8 hours", correct: false },
-//           { text: "12-24 hours", correct: false },
-//           { text: "36-48 hours", correct: true, },
-//           { text: "72-96 hours", correct: false },
-//         ],
-//     },
-//     {
-//         question: "Which of the following is true about sloths fur?",
-    
-//         answers: [
-//           { text: "It is naturally blue in colour", correct: false },
-//           { text: "It grows in the opposite direction to other mammals", correct: true },
-//           { text: "It sheds once a year", correct: false },
-//           { text: "It is resistant to parasites", correct: false },
-//         ],
-//     },
-  
-//     {
-//         question: "What is the weight of a newborn sloth?",
-    
-//         answers: [
-//           { text: "1-2 pounds", correct: true },
-//           { text: "3-5 pounds", correct: false },
-//           { text: "6-8 pounds", correct: false },
-//           { text: "9-11 pounds", correct: false },
-//         ],
-//     },
-  
-//     {
-//         question: "Who is a natural predator of sloths?",
-    
-//         answers: [
-//           { text: "jaguars", correct: true },
-//           { text: "elephants", correct: false },
-//           { text: "crocodiles", correct: false },
-//           { text: "gorillas", correct: false },
-//         ],
-//     },
-  
-//     {
-//         question: "Where are sloths found in the wild?",
-    
-//         answers: [
-//           { text: "Africa", correct: false },
-//           { text: "Asia", correct: false },
-//           { text: "Australia", correct: false },
-//           { text: "Central and South America", correct: true },
-//         ],
-//     },
-  
-//     {
-//         question: "How many hours a day do sloths sleep?",
-    
-//         answers: [
-//           { text: "8-10 hours", correct: false },
-//           { text: "10-12 hours", correct: false },
-//           { text: "12-14 hours", correct: true },
-//           { text: "14-16 hours", correct: false },
-//         ],
-//     },
-  
-//     {
-//         question: "How many chambers does a sloth stomach have?",
-    
-//         answers: [
-//           { text: "one", correct: false },
-//           { text: "two", correct: true },
-//           { text: "three", correct: false },
-//           { text: "four", correct: false },
-//         ],
-//     },
-  
-//     {
-//         question: "Which sense is the best for sloths?",
-    
-//         answers: [
-//           { text: "sight", correct: false },
-//           { text: "taste", correct: false },
-//           { text: "touch", correct: false },
-//           { text: "smell", correct: true },
-//         ],
-//     },
-//     {
-//         question: "What is the main diet of a sloth?",
-    
-//         answers: [
-//           { text: "insects", correct: false },
-//           { text: "meat", correct: false },
-//           { text: "leaves", correct: true },
-//           { text: "fish", correct: false },
-//         ],
-//     }
-//   ]
--- js/script.js
+++ js/script.js
@@ -1,114 +1,328 @@
-//Selecting required elements //
-const startBtn = document.querySelector('.start-btn');
-const infoBox = document.querySelector('.info-box');
-const exitBtn = document.querySelector('.buttons .quit');
-const continueBtn = document.querySelector('.buttons .restart');
-const quizBox = document.querySelector('.quiz-box');
-const resultBox = document.querySelector('.result.box');
-const optionList = document.querySelector('.option-list');
-const timeLine = document.querySelector('.timer .time-line');
-const timeText = document.querySelector('.timer .time-left-text');
-const timeCount = document.querySelector('.timer .timer-sec');
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
 
-//If start button is clicked//
-startBtn.onclick = () => {
- infoBox.classList.add('activeInfo'); //show info box//
+// if startQuiz button clicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //show info box
 }
 
-//If exit button is clicked//
-exitBtn.onclick = () => {
-    infoBox.classList.remove('activeInfo'); //hide info box//
+// if exitQuiz button clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
 }
 
-//If continue button is clicked//
-continueBtn.onclick = () => {
-    infoBox.classList.remove('activeInfo'); //hide info box//
-    quizBox.classList.add('activeQuiz'); //show quiz box //
-    showQuestions(0); //calling show questions function //
+// if continueQuiz button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
     startTimer(15); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function 
+    startTimerLine(0); //calling startTimerLine function
 }
 
-let timeValue = 15; //Time to answer each question//
-let queCount = 0;
-let queNumb = 1;
+let timeValue =  15;
+let que_count = 0;
+let que_numb = 1;
 let userScore = 0;
 let counter;
 let counterLine;
 let widthValue = 0;
 
-const restartQuiz = resultBox.querySelector('.buttons .restart');
-const quitQuiz = resultBox.querySelector('.buttons .quit');
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
 
-//if restart button is clicked//
-restartQuiz.onclick = () => {
-    quizBox.classList.add('activeQuiz');
-    resultBox.classList.remove('activeResult');
-    timeValue = 15;
-    queCount = 0;
-    queNumb = 1;
+// if restartQuiz button clicked
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    result_box.classList.remove("activeResult"); //hide result box
+    timeValue = 15; 
+    que_count = 0;
+    que_numb = 1;
     userScore = 0;
     widthValue = 0;
-    showQuestions(queCount); //calling show questions function//
-    queCounter(queNumb); //passing queNumb value to queCounter//
-    clearInterval(counter); //clear Counter
-    clearInterval(counterLine); //clear Counter Line
-    startTimer(timeValue); //calling startTime function
+    showQuetions(que_count); //calling showQestions function
+    queCounter(que_numb); //passing que_numb value to queCounter
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    startTimer(timeValue); //calling startTimer function
     startTimerLine(widthValue); //calling startTimerLine function
-    timeText.textContent = 'Time left'; // Change the text of timeText to Time left
-    nextBtn.classList.remove('show'); //hide the next button
+    timeText.textContent = "Time Left"; //change the text of timeText to Time Left
+    next_btn.classList.remove("show"); //hide the next button
 }
 
-//if quit quiz button is clicked//
-quitQuiz.onclick = () => {
+// if quitQuiz button clicked
+quit_quiz.onclick = ()=>{
     window.location.reload(); //reload the current window
 }
 
-const nextBtn = document.querySelector('footer .next-btn');
-const queCounter = document.querySelector('footer .totalQue');
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
 
-//if next Btn is clicked//
-nextBtn.onclick = () => {
-    if(queCount < questions.length -1){ //if question count is less than total question length
-        queCount++; //increment the queCount value
-        queNumb++; //increment the queNumb value
-        showQuestions(queCount); //calling showQuestions function
-        queCounter(queNumb); //passing the queNumb value to queCounter
+// if Next Que button clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ //if question count is less than total question length
+        que_count++; //increment the que_count value
+        que_numb++; //increment the que_numb value
+        showQuetions(que_count); //calling showQestions function
+        queCounter(que_numb); //passing que_numb value to queCounter
         clearInterval(counter); //clear counter
         clearInterval(counterLine); //clear counterLine
         startTimer(timeValue); //calling startTimer function
         startTimerLine(widthValue); //calling startTimerLine function
-        timeText.textContent = 'Time left'; //change the Timetext to Time left
-        nextBtn.classList.remove('show'); //hide the next button      
-    } else {
+        timeText.textContent = "Time Left"; //change the timeText to Time Left
+        next_btn.classList.remove("show"); //hide the next button
+    }else{
         clearInterval(counter); //clear counter
         clearInterval(counterLine); //clear counterLine
         showResult(); //calling showResult function
     }
 }
 
-//getting questions and options from array//
-function showQuestions(index) {
-    const queText = document.querySelector('.que-text');
+// getting questions and options from array
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
 
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    
+    const option = option_list.querySelectorAll(".option");
 
-//creating a new span and div for questions and options and passing the value using an array//
-let queTag = '<span>' + questions[index].numb + '. ' + questions[index].question +'</span>';
-let optionTag = '<div class='option><span>' + questions[index].options[0] + '</span></div>'
-+ '<div class='option'><span>' + questions[index].options[1] + '</span></div>'
-+ '<div class='option'><span>' + questions[index].options[2] + '</span></div>'
-+ '<div class='option'><span>' + questions[index].options[3] + '</span></div>'
-queText.innerHTML = QueTag;  //adding new span tag inside option tag//
-optionList.innerHTML = optionTag; //adding new div tag inside option tag
+    // set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+// creating the new div tags which for icons
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
-const option = optionList.querySelectorAll('.option');
+//if user clicked on option
+function optionSelected(answer){
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    let userAns = answer.textContent; //getting user selected option
+    let correcAns = questions[que_count].answer; //getting correct answer from array
+    const allOptions = option_list.children.length; //getting all option items
+    
+    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
+        userScore += 1; //upgrading score value with 1
+        answer.classList.add("correct"); //adding green color to correct selected option
+        answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); //adding red color to correct selected option
+        answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+        console.log("Wrong Answer");
 
-//set onclick attribute to all available options//
-for(i=0; i < option.length; i++) {
-    option[i].setAttribute('onclick', 'optionSelected(this)');
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer 
+                option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+    }
+    next_btn.classList.add("show"); //show the next button if user selected any option
 }
 
-//creating the new div tags for icons//
-let tickIconTag = '<div class = 'icon tick'><i class="fa-solid fa-check"></i></div>';
-let crossIconTag = '<div class = 'icon cross'><i class="fa-solid fa-xmark"></i></div>';
+function showResult(){
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.remove("activeQuiz"); //hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore > 3){ // if user scored more than 3
+        //creating a new span tag and passing the user score number and total question number
+        let scoreTag = '<span>and congrats! ����, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+    }
+    else if(userScore > 1){ // if user scored more than 1
+        let scoreTag = '<span>and nice ����, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{ // if user scored less than 1
+        let scoreTag = '<span>and sorry ����, You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time; //changing the value of timeCount with time value
+        time--; //decrement the time value
+        if(time < 9){ //if timer is less than 9
+            let addZero = timeCount.textContent; 
+            timeCount.textContent = "0" + addZero; //add a 0 before time value
+        }
+        if(time < 0){ //if timer is less than 0
+            clearInterval(counter); //clear counter
+            timeText.textContent = "Time Off"; //change the time text to time off
+            const allOptions = option_list.children.length; //getting all option items
+            let correcAns = questions[que_count].answer; //getting correct answer from array
+            for(i=0; i < allOptions; i++){
+                if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
+                    option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+                    console.log("Time Off: Auto selected correct answer.");
+                }
+            }
+            for(i=0; i < allOptions; i++){
+                option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+            }
+            next_btn.classList.add("show"); //show the next button if user selected any option
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1; //upgrading time value with 1
+        time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+        if(time > 549){ //if time value is greater than 549
+            clearInterval(counterLine); //clear counterLine
+        }
+    }
+}
+
+function queCounter(index){
+    //creating a new span tag and passing the question number and total question
+    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
+}
+
+// let questions = [
+//     {
+//         numb:1
+//           question: 'What is the average lifespan of sloths in the wild?',
+//           answer: '20-25 years',
+//           options: [
+//             '15-20 years',
+//             '20-25 years',
+//             '30-35 years',
+//             '40-45 years'
+//           ]
+//       },
+//       {
+//         numb:2
+//           question: 'How many hours does it take for a sloth to digest a meal?',
+//           answer: '36-48 hours',
+//           options: [
+//             '6-8 hours',
+//             '12-24 hours',
+//             '36-48 hours', 
+//             '72-96 hours'
+//           ]
+//       },
+//       {
+//         numb:3
+//           question: 'Which of the following is true about sloths fur?',
+//           answer: 'It grows in the opposite direction to other mammals',
+//           options: [
+//             'It is naturally blue in colour',
+//             'It grows in the opposite direction to other mammals',
+//             'It sheds once a year',
+//             'It is resistant to parasites'
+//           ]
+//       }, 
+//       {
+//         numb:4
+//           question: 'What is the weight of a newborn sloth?',
+//           answer: '1-2 pounds',
+//           options: [
+//             '1-2 pounds',
+//             '3-5 pounds', 
+//             '6-8 pounds',
+//             '9-11 pounds'
+//           ]
+//       },
+//       {
+//         numb:5
+//           question: 'Who is a natural predator of sloths?',
+//           answer: 'jaguars',
+//           options: [
+//             'jaguars',
+//             'elephants',
+//             'crocodiles',
+//             'gorillas'
+//           ]
+//       },
+//       {
+//         numb:6
+//           question: 'Where are sloths found in the wild?',
+//           answer: 'Central and South America',
+//           options: [
+//             'Africa'
+//             'Asia'
+//             'Australia'
+//             'Central and South America'
+//           ]
+//       },
+//       {
+//         numb:7
+//           question: 'How many hours a day do sloths sleep?',
+//           answer: '12-14 hours',
+//           options: [
+//             '8-10 hours',
+//             '10-12 hours',
+//             '12-14 hours', 
+//             '14-16 hours'
+//           ]
+//       },
+//       {
+//         numb:8
+//           question: 'How many chambers does a sloth stomach have?',
+//           answer: 'two',
+//           options: [
+//             'one', 
+//             'two', 
+//             'three', 
+//             'four'
+//           ]
+//       },
+//       {
+//         numb:9
+//           question: 'Which sense is the best for sloths?',
+//           answer: 'smell'
+//           options: [
+//             'sight',
+//             'taste',
+//             'touch',
+//             'smell'
+//           ]
+//       },
+//       {
+//         numb:10
+//           question: 'What is the main diet of a sloth?',
+//           answer: 'leaves',
+//           options:
+//           [
+//             'insects',
+//             'meat',
+//             'leaves',
+//             'fish'
+//           ]
+//       }
+//     ]
+  
+  
